be3800ecb7c7424a3d704ddad32de025882c4da3
geneziolavor <gegeitp@gmail.com>
2025-10-03 22:18:58 -0300

    Limpeza final de submódulos antigos

--- Documents/sistema_correcao_provas_v2/build/web/flutter_service_worker.js
+++ Documents/sistema_correcao_provas_v2/build/web/flutter_service_worker.js
@@ -28,7 +28,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "f5a0c4f6737c94cfe77559f5ebd953ca
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "b5e096bd93d927c040b4e9e151539953",
+"flutter_bootstrap.js": "5236b5a411f6b430664fbd768bf9625e",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
